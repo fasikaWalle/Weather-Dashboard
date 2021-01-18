2ac6cec8f5d61dd1e0892c3a9b12c4a16ba4f7a5
fasikaWalle <fasikabini12@gmail.com>
2021-01-18 12:54:58 -0600

    /display 5 day weather status

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,3 +168,137 @@ function saveWeatherInfo(weatherInfo) {
   console.log(weatherInfo);
   localStorage.setItem("weather-info", JSON.stringify(weatherInfo));
 }
+function displayWeatherStatus(weatherInfo) {
+  console.log(weatherInfo);
+  cityNameE1.textContent = "";
+  currentWeatherInfoE1.textContent = "";
+  futureWeatherContainer.textContent = "";
+  console.log(weatherInfo);
+  var currentDate = weatherInfo.weather[0].date;
+
+  currentDate = moment(currentDate).format("L");
+
+  var weatherIcon = weatherInfo.weather[0].icon;
+  var city = weatherInfo.name;
+
+  console.log(city);
+
+  var weatherIconI = document.createElement("img");
+  weatherIconI.className = "fs-3 text";
+  weatherIconI.src = "http://openweathermap.org/img/wn/" + weatherIcon + ".png";
+  // document.append(weatherIconI);
+
+  cityNameE1.innerHTML =
+    "<span>" + city + " " + "&#40;" + currentDate + "&#41</span>";
+  cityNameE1.append(weatherIconI);
+  // tempSpanE1 = document.createElement("span");
+  // tempSpanE1.innerHTML = "<span>" + temp + "&#8457;" + "</span>";
+  var currentDayWeather = {
+    Temprature: weatherInfo.weather[0].temp,
+    humidity: weatherInfo.weather[0].humidity,
+    WindSpeed: weatherInfo.weather[0].speed,
+    UvIndex: weatherInfo.weather[0].Uvindex,
+  };
+
+  // weatherInfo[0].push(city);
+  // weatherInfo.city.push(currentDayWeather);
+  var index = -1;
+  for (var i in currentDayWeather) {
+    index++;
+    listCurrentWeaterE1 = document.createElement("li");
+
+    listCurrentWeaterE1.className = "list-unstyled list-group-item border-0";
+    listCurrentWeaterE1.innerHTML =
+      i + ":" + "<span>" + currentDayWeather[i] + "</span>";
+
+    // index++;
+    console.log(currentDayWeather[i]);
+    if (index === 0) {
+      var degreeF = document.createElement("span");
+      degreeF.innerHTML = "<span>&#8457;</span>";
+      listCurrentWeaterE1.appendChild(degreeF);
+    }
+    if (index === 3) {
+      var uvIndex = Math.floor(currentDayWeather[i]);
+
+      if (uvIndex >= 0 && uvIndex <= 2) {
+        listCurrentWeaterE1.className = "green";
+      } else if (uvIndex >= 3 && uvIndex <= 5) {
+        listCurrentWeaterE1.className = "yellow";
+      } else if (uvIndex >= 6 && uvIndex <= 7) {
+        listCurrentWeaterE1.className = "orange";
+      } else if (uvIndex >= 8 && uvIndex <= 10) {
+        listCurrentWeaterE1.className = "red";
+      } else if (uvIndex >= 10) {
+        listCurrentWeaterE1.className = "pink";
+      }
+    }
+    currentWeatherInfoE1.appendChild(listCurrentWeaterE1);
+  }
+
+  displayFutureWeather(weatherInfo);
+}
+// weatherInfo.forEach((item) => {
+//   for (const property in item) {
+//     console.log(item[property]);
+//   }
+// });
+
+//display 5 day weather status
+function displayFutureWeather(weatherInfo) {
+  for (var i = 1; i < 6; i++) {
+    // var UvIndex
+    var date = weatherInfo.weather[i].date;
+    date = moment(date).format("L");
+    // var sunnyEmoji=data[i]
+    var temp = weatherInfo.weather[i].temp;
+    var humidity = weatherInfo.weather[i].humidity;
+    // var futureForcast = { date: date, temp: temp, humidity: humidity };
+    // var listE1 = document.createElement("div");
+    // listE1.className = "card";
+
+    var cardWrapper = document.createElement("div");
+
+    cardWrapper.className = "col card-body";
+    // var cardBody = document.createElement("div");
+    // cardBody.className = "";
+
+    var headerE1 = document.createElement("h4");
+    headerE1.className = "";
+    headerE1.textContent = date;
+
+    var unorderedListE2 = document.createElement("p");
+    unorderedListE2.className = "list-unstyled";
+    unorderedListE2.textContent = temp;
+
+    var listIcon = document.createElement("p");
+
+    if (temp >= 65) {
+      listIcon.className = "ion-ios7-sunny list-unstyled";
+    } else if (Math.floor(temp) >= 45 && Math.floor(temp) < 65) {
+      listIcon.className = "ion-ios7-partlysunny  list-unstyled";
+    } else if (Math.floor(temp) >= 25 && Math.floor(temp) <= 35) {
+      listIcon.className = "cloudy list-unstyled";
+    } else if (Math.floor(temp) < 25) {
+      listIcon.className = "ion-ios7-rainy   list-unstyled";
+    }
+
+    // <i class="ion-ios7-sunny"></i>
+    // <i class="ion-ios7-partlysunny"></i>
+    // <i class="ion-ios7-rainy"></i>
+
+    var degreeF = document.createElement("span");
+    degreeF.innerHTML = "<span>&#8457;</span>";
+    unorderedListE2.appendChild(degreeF);
+
+    var listaE1 = document.createElement("li");
+    listaE1.textContent = humidity;
+    listaE1.className = "card-text list-unstyled";
+    cardWrapper.appendChild(headerE1);
+    cardWrapper.appendChild(unorderedListE2);
+    cardWrapper.appendChild(listIcon);
+    cardWrapper.appendChild(listaE1);
+
+    futureWeatherContainer.appendChild(cardWrapper);
+  }
+}
